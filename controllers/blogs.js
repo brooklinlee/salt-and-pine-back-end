de402d406172d440b7ab269109be447900cbae54
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2023-12-05 12:12:44 -0700

    work to debug deleted comment still showing until refresh

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -103,15 +103,14 @@ async function updateComment (req, res) {
   }
 }
 
-async function deleteComment(req, res) {
+const deleteComment = async (req, res) => {
   try {
     const blog = await Blog.findById(req.params.blogId)
-    blog.comments.remove({_id: req.params.commentId})
+    blog.comments.remove({ _id: req.params.commentId })
     await blog.save()
     res.status(200).json(blog)
-  } catch (error) {
-    console.log('❌', error)
-    res.status(500).json(error)
+  } catch (err) {
+    res.status(500).json(err)
   }
 }
 
